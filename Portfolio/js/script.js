43f76a6d4b215f285e5d75fe9403ba73c8cf2321
dodosiz <asvestopouli@hotmail.com>
2017-03-05 21:08:39 +0200

    Added images

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -3,17 +3,30 @@ $(document).ready(function(){
     // Initially those elements don't show
     $("a.column").hide();
     $("a.button").hide();
+    $("#portfolio").hide();
     $("#welcome").hide();
     $("#welcome_p").hide();
+    $("section.contact").hide();
+    $("#about").hide();
+    $("hr").hide();
+    $(".portfolio").hide();
 
+    $(document).scroll(function(){
+        //$("a.column").fadeIn(3000);
+        $("#portfolio").fadeIn(3000);
+        $("section.contact").fadeIn(4000);
+        $("hr").fadeIn(3000);
+    });
+    
     $(document).click(function(){
         // Animations while 
+        $(".portfolio").show();
         $("#just_click").hide();
         $("#welcome").fadeIn(500);
         $("#welcome_p").fadeIn(750);
         $("#twitter").fadeIn(1000);
         $("#linkdn").fadeIn(1500);
         $("#github").fadeIn(2000);
-        $("a.column").slideDown("slow");
+        $("#about").slideDown(2000);
     });
 });
