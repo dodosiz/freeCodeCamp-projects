080999961f75e826ed8cdf8ee2921181a7dabaab
dodosiz <asvestopouli@hotmail.com>
2017-03-05 21:27:22 +0200

    Put more comments

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,32 +1,67 @@
 $(document).ready(function(){
   
-    // Initially those elements don't show
-    $("a.column").hide();
+    /* ************************************
+    Initially these elements are hidden
+    ************************************* */
+    
+    // social media buttons
     $("a.button").hide();
-    $("#portfolio").hide();
+    
+    // the main header and subheader
     $("#welcome").hide();
     $("#welcome_p").hide();
+    
+    // contact form
     $("section.contact").hide();
+    
+    // description about yourself
     $("#about").hide();
+    
+    // this is the horizontal rule in the middle
     $("hr").hide();
+    
+    // the portfolio projects
+    $("#portfolio").hide();
+    
+    // this is the parrent of the actual portfolio
     $(".portfolio").hide();
 
+    
+    /* ************************************
+    These elements show after scroll
+    ************************************* */
     $(document).scroll(function(){
-        //$("a.column").fadeIn(3000);
+        // project icons fade in
         $("#portfolio").fadeIn(3000);
+        
+        // contact form fades in
         $("section.contact").fadeIn(4000);
+        
+        // this is the horizontal rule in the middle
         $("hr").fadeIn(3000);
     });
     
+    
+    /* ************************************
+    These elements show after click
+    ************************************* */
     $(document).click(function(){
-        // Animations while 
+        // this is to allow the projects to show
         $(".portfolio").show();
+        
+        // the click anywhere message
         $("#just_click").hide();
+        
+        // the main header and subheader
         $("#welcome").fadeIn(500);
         $("#welcome_p").fadeIn(750);
+        
+        // social media buttons
         $("#twitter").fadeIn(1000);
         $("#linkdn").fadeIn(1500);
         $("#github").fadeIn(2000);
+        
+        // the description about yourself
         $("#about").slideDown(2000);
     });
 });
